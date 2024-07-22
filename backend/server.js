ce58006fe19076b121cf4a62ba8802e3f974a1ed
Piyushh-h <piyushsinha0810@gmail.com>
2024-07-23 00:25:47 +0530

    packages

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-/* 3:31*/
+import express from "express" 
+import cors from "cors" 
+
+
+
+// app config 
+const app = express() 
+const port = 4000 
+
+// midddleware 
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req,res) => {
+    res.send("API Working")
+})
+
+app.listen(port,()=>{
+    console.log('Server Started on http://localhost:${port}')
+})
